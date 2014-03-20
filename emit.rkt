9c7c58a490925fd557910fbc963cc661a1ca6015
Shriram Krishnamurthi <sk@cs.brown.edu>
2014-03-20 15:17:55 -0400

    links to contributed content

--- emit.rkt
+++ emit.rkt
@@ -9,13 +9,15 @@
 (define f (parse-tsv/file (build-path "metadata" "summary.tsv")))
 (define g (convert-to-struct (strip-header f)))
 
-(define l1 (builds g));(take (builds g) 5))
-(define l2 (build-fails g)); (take (build-fails g) 5))
+(define l1 (builds g))
+;(define l1 (take (builds g) 5))
+(define l2 (build-fails g))
+;(define l2 (take (build-fails g) 5))
 
 (define the-doc (generate-document l1 l2))
 
 (render (list the-doc) 
         (list "index.html")
-        #:dest-dir "data"
+        #:dest-dir output-dir
         #:style-extra-files (list "style-changes.css")
         )
--- gen-doc.rkt
+++ gen-doc.rkt
@@ -5,7 +5,13 @@
 (require scribble/base)
 (require scribble/decode)
 
-(provide generate-document)
+(provide generate-document output-dir)
+
+(define output-dir "data")
+
+;; Note: output goes to a output subdir, so the link doesn't need to include it
+;; (because it's automatically the base directory) whereas the check in the
+;; filesystem needs to include it (because it's not).
 
 (define (make-link sub-paths file-name)
   (cond
@@ -13,6 +19,9 @@
     [(cons? sub-paths)
      (string-append (first sub-paths) "/" 
                     (make-link (rest sub-paths) file-name))]))
+
+(define (make-file-name path file)
+  (apply build-path (cons output-dir (append path (list file)))))
 
 (define (generate-paper-list ps)
   (tabular #:style 'boxed #:sep (hspace 1)
@@ -22,7 +31,22 @@
                 (paper-title p)
                 (hyperlink 
                  (make-link (paper-path p) "build_notes.txt")
-                 "notes")))
+                 "notes")
+                (let ([dispute-file (make-file-name (paper-path p) "dispute.txt")]
+                      [cleared-file (make-file-name (paper-path p) "cleared.txt")]
+                      [problem-file (make-file-name (paper-path p) "problem.txt")])
+                  (list
+                   (if (file-exists? dispute-file)
+                       (hyperlink (make-link (paper-path p) "dispute.txt") "dispute!")
+                       " ")
+                   (linebreak)
+                   (if (file-exists? cleared-file)
+                       (hyperlink (make-link (paper-path p) "cleared.txt") "cleared?")
+                       " ")
+                   (linebreak)
+                   (if (file-exists? problem-file)
+                       (hyperlink (make-link (paper-path p) "problem.txt") "problem?")
+                       " ")))))
         ps)))
 
 (define (generate-document bs bfs)
