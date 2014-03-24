8ad2fd7bdd8fc3b0c17917c017965df74b706a04
Shriram Krishnamurthi <sk@cs.brown.edu>
2014-03-23 20:26:05 -0400

    two colors to indicate difference in findings

--- gen-doc.rkt
+++ gen-doc.rkt
@@ -7,17 +7,32 @@
 (require scribble/core)
 (require scribble/base)
 (require scribble/decode)
+(require scribble/html-properties)
 (require file/sha1)
 
 (provide generate-document)
 
-(define (generate-paper-list ps color)
+(define (generate-paper-list ps left-color right-color)
   (if (empty? ps)
       (para "Nothing here!")
       (tabular 
-       #:style (style #f
-                      (list (background-color-property color)))
        #:sep (hspace 1)
+       #:style (style #f
+                      (list (table-columns (list (style #f '(right)) ;; NB: the style properties are found in table-cells
+                                                 (style #f '(center))
+                                                 (style #f '(left))
+                                                 (style #f '(center))
+                                                 (style #f '(left))
+                                                 (style #f '(center))
+                                                 (style #f (list 'center
+                                                                 (attributes
+                                                                  `((style . ,(string-append "background-color: #"
+                                                                                             (color-list->color-string left-color)))))))
+                                                 (style #f '(center))
+                                                 (style #f (list 'center
+                                                                 (attributes
+                                                                  `((style . ,(string-append "background-color: #"
+                                                                                             (color-list->color-string right-color)))))))))))
        (map (lambda (p)
               (list (paper-group p)
                     (paper-authors p)
@@ -37,6 +52,7 @@
                      (if (problem? p)
                          (hyperlink (problem-link (paper-path p)) "problem?")
                          " ")
+                     (linebreak)
                      (if (misclassified? p)
                          (hyperlink (misclass-link (paper-path p)) "misclassified")
                          " ")
@@ -45,43 +61,48 @@
 
 (define (color-string->color-list s)
   (bytes->list (hex-string->bytes s)))
+(define (color-list->color-string l)
+  (bytes->hex-string (list->bytes l)))
 
-#| Colors from http://www.colourlovers.com/palette/3289630/Easter_Glory |#
-(define bad-color (color-string->color-list "FB755F"))
+#| Colors from http://www.colourlovers.com/palette/3292950/Candy_colors by averagegirl |#
+
+(define bad-color (color-string->color-list "F2BAD6"))
 (define good-color (color-string->color-list "6BEEE2"))
-(define neutral-color (color-string->color-list "B297F4"))
+(define neutral-color (color-string->color-list "D1FCFC"))
+(define progress-color (color-string->color-list "BEE2F4"))
+(define misclass-color (color-string->color-list "DDB7E2"))
 
-(struct sec (title filter color))
+(struct sec (title filter left-col-color right-col-color))
 
 (define report-sections
   (list (sec "Purported Not Building; Disputed; Not Checked"
-             (and-filters not-building? disputed? not-checked?) neutral-color)
+             (and-filters not-building? disputed? not-checked?) bad-color neutral-color)
         (sec "Purported Building; Disputed; Not Checked"
-             (and-filters building? disputed? not-checked?) neutral-color)
+             (and-filters building? disputed? not-checked?) good-color neutral-color)
         (sec "Conflicting Checks!"
-             (and-filters cleared? problem?) bad-color)
+             (and-filters cleared? problem?) neutral-color bad-color)
         (sec "Misclassified"
-             misclassified? neutral-color)
+             misclassified? neutral-color misclass-color)
         ;; don't use disputed? for the next two, because people may have checked
         ;; without a formal dispute filed!
         (sec "Purported Not Building But Found Building"
-             (and-filters not-building? cleared? not-problem?) good-color)
+             (and-filters not-building? cleared? not-problem?) bad-color good-color)
         (sec "Purported Building But Found Not Building"
-             (and-filters building? not-cleared? problem?) bad-color)
+             (and-filters building? not-cleared? problem?) good-color bad-color)
         (sec "Purported Not Building; Confirmed"
-             (and-filters not-building? not-cleared? problem?) good-color)
+             (and-filters not-building? not-cleared? problem?) bad-color bad-color)
         (sec "Purported Building; Confirmed"
-             (and-filters building? cleared? not-problem?) good-color)
+             (and-filters building? cleared? not-problem?) good-color good-color)
         (sec "All Others Purported Not Building"
-             (and-filters not-building? not-misclassified? not-disputed? not-checked? not-problem?) bad-color)
+             (and-filters not-building? not-misclassified? not-disputed? not-checked? not-problem?) bad-color neutral-color)
         (sec "All Other Purported Building"
-             (and-filters building? not-misclassified? not-disputed? not-checked? not-problem?) good-color)))
+             (and-filters building? not-misclassified? not-disputed? not-checked? not-problem?) good-color neutral-color)))
 
 (define (generate-document papers)
   (define (gen-filtered f color) (generate-paper-list (filter f papers) color))
-  (define (make-section title relevant-papers color)
+  (define (make-section title relevant-papers left-color right-color)
     (list (section title " (" (number->string (length relevant-papers)) ")")
-          (generate-paper-list (shuffle relevant-papers) color)))
+          (generate-paper-list (shuffle relevant-papers) left-color right-color)))
   (define paper-count (length papers))
   (decode
    (list
@@ -91,7 +112,7 @@
     (section "Progress")
     (tabular #:sep (hspace 1)
              #:style (style #f
-                            (list (background-color-property neutral-color)
+                            (list (background-color-property progress-color)
                                   (table-columns (list (style #f '(right)) ;; NB: the style properties are found in table-cells
                                                        (style #f '(center))
                                                        (style #f '(right))
@@ -112,7 +133,7 @@
     review-protocol
     review-format
     (map (lambda (s)
-           (make-section (sec-title s) (filter (sec-filter s) papers) (sec-color s)))
+           (make-section (sec-title s) (filter (sec-filter s) papers) (sec-left-col-color s) (sec-right-col-color s)))
          report-sections)
     (section "Threats to Validity")
     threats-to-validity
